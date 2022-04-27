a5416b525178d8647d04b2c8a8010d4b4a3b7884
bassie.bangura <bassiebangura@ramseysolutions.com>
2022-04-27 11:10:24 -0500

    Add elements for writing reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,4 +4,26 @@ class ReviewsController < ApplicationController
         arrayOfReviews = @movie.reviews
         @reviews = arrayOfReviews
      end
+     def show
+        @movie = Movie.find(params[:movie_id])
+    end
+    def new
+        @movie = Movie.find(params[:movie_id])
+        @review = @movie.reviews.new
+    end
+    def create
+        @movie = Movie.find(params[:movie_id])
+        @review = @movie.reviews.new(review_params)
+        
+        if @review.save
+            redirect_to movie_reviews_path(@movie),
+            notice: "Thanks for your review!"
+        else
+            render :new
+        end
+    end
+    private
+    def review_params
+        params.require(:review).permit(:name, :comment, :stars)
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :movie
+  def stars_as_percent
+    (stars / 5.0) * 100.0
+  end
 end
