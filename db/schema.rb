a3225bd75fecc2aa776b1264c8a62a35035d0fa3
bassie.bangura <bassiebangura@ramseysolutions.com>
2022-04-29 14:59:13 -0500

    add unique username field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_27_204745) do
+ActiveRecord::Schema.define(version: 2022_04_29_193013) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2022_04_27_204745) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "reviews", "movies"
