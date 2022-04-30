0e269f257530821984f61b4436b3ed4814c1e42e
bassie.bangura <bassiebangura@ramseysolutions.com>
2022-04-30 05:38:28 -0500

    update pkgs and create controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,16 @@ class SessionsController < ApplicationController
 
     end
     def create
-        user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
-        if user && user.authenticate(params[:password])
-            session[:user_id] = user.id
-            redirect_to movies_url, notice: "Welcome Back"
-        else
-          flash.now[:notice] = "Invalid email/password"
-          render :new
-        end
+       @user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+    
+       if @user && user.authenticate(params[:password])
+     
+           session[:user_id] = @user.id
+           redirect_to users_path
+       else
+           flash.now[:alert] = "Log in failed..."
+           render :new
+       end
     end
     def destroy
         session[:user_id] = nil
@@ -18,3 +20,12 @@ class SessionsController < ApplicationController
     end
 
 end
+
+# user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+# if user && user.authenticate(params[:password])
+#     session[:user_id] = user.id
+#     redirect_to movies_url, notice: "Welcome Back"
+# else
+#   flash.now[:notice] = "Invalid email/password"
+#   render :new
+# end
