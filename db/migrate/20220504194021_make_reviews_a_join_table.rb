73400bbe3b292cd94dca467d2ac0ee43869850d7
bassie.bangura <bassiebangura@ramseysolutions.com>
2022-05-05 08:20:38 -0500

    Add user_id in new Reviews table update

--- db/migrate/20220504194021_make_reviews_a_join_table.rb
+++ db/migrate/20220504194021_make_reviews_a_join_table.rb
@@ -1,4 +1,9 @@
 class MakeReviewsAJoinTable < ActiveRecord::Migration[6.1]
   def change
+    remove_column :reviews, :name
+    add_column :reviews, :user_id, :integer 
+    Review.delete_all
   end
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_02_153343) do
+ActiveRecord::Schema.define(version: 2022_05_04_194021) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -23,12 +23,12 @@ ActiveRecord::Schema.define(version: 2022_05_02_153343) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.string "name"
     t.integer "stars"
     t.text "comment"
     t.integer "movie_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["movie_id"], name: "index_reviews_on_movie_id"
   end
 
