cdbfa11f49d82a7cbd6c9c955f8c43b4e1a9b647
bassie.bangura <bassiebangura@ramseysolutions.com>
2022-05-06 10:25:55 -0500

    run favorite table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_04_194021) do
+ActiveRecord::Schema.define(version: 2022_05_06_150628) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_favorites_on_movie_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
@@ -43,5 +52,7 @@ ActiveRecord::Schema.define(version: 2022_05_04_194021) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "favorites", "movies"
+  add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "movies"
 end
