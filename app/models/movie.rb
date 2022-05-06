40cede2d85a4abca87163880d9da24eb4729537d
bassie.bangura <bassiebangura@ramseysolutions.com>
2022-05-06 10:36:12 -0500

    Add movie and user has_many relationships for favorite

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
     has_many :reviews, dependent: :destroy
+    has_many :favorites, dependent: :destroy
     def flopper?
         total_gross.blank? || total_gross < 500000000
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  has_many: favorites, dependent: :destrouy
   has_secure_password
   validates :name, presence: true
   validates :email, format: {with: /\S+@\S+/}
