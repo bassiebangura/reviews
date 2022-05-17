6a3eb23c312b6be7a2488e653eab2e83213756c1
bassie.bangura <bassiebangura@ramseysolutions.com>
2022-05-17 10:36:09 -0500

    Refactor fave_or_unfave to a helper

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,19 @@
 class FavoritesController < ApplicationController
     before_action :require_signin, except: [:index, :show]
-    
+    before_action :set_movie, only: [:create, :destroy]
     def create
-        movie = Movie.find(params[:movie_id])
-        movie.fans << current_user
-        redirect_to movie
+        set_movie.fans << current_user
+        redirect_to set_movie
     end
     def destroy
+        
         favorite = current_user.favorites.find(params[:id])
         favorite.destroy
-
-        movie = Movie.find(params[:movie_id])
-        redirect_to movie
+        redirect_to set_movie
     end
  
+    private
+    def set_movie
+        @movie = Movie.find(params[:movie_id])
+    end
 end
